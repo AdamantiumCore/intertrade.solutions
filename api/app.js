752db6be530bb998bdcc546b92fdb096abb716ab
Gogi4aaa <georgi-07@abv.bg>
2024-05-22 20:29:05 +0300

    set up auth controller

--- api/app.js
+++ api/app.js
@@ -2,7 +2,9 @@ import express from "express";
 import authRoute from "./routes/auth.route.js";
 const app = express();
 
-app.use("/api/auths", authRoute)
+app.use(express.json())
+
+app.use("/api/auth", authRoute)
 
 app.listen(8800, () => {
     console.log("Server is running!");
---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+export const register = (req,res)=>{
+    //db operations
+    console.log("register")
+}
+export const login = (req,res)=>{
+    //db operations
+}
+export const logout = (req,res)=>{
+    //db operations
+}
