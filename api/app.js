f3933c339e589193095d738595dda30548485944
Gogi4aaa <georgi-07@abv.bg>
2024-05-24 18:42:47 +0300

    resolved conflict with docker file and change requests url to /api/v1/auth

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json())
 
-app.use("/api/auth", authRoute)
+app.use("/api/v1/auth", authRoute)
 
 app.listen(8800, () => {
     console.log("Server is running!");
