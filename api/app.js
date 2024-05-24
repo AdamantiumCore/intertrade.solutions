fe39544c711b92dff77cd6f4b20c001c958831f9
Gogi4aaa <georgi-07@abv.bg>
2024-05-24 18:48:14 +0300

    resolved conflict

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser())
 
-app.use("/api/auth", authRoute)
+app.use("/api/v1/auth", authRoute)
 
 app.listen(8800, () => {
     console.log("Server is running!");
