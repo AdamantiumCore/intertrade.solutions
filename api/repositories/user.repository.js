425a20bfff19051ffd95284615a77428ccd377e1
Noman Ali Syed <nalisyedn@gmail.com>
2024-05-26 15:25:07 +0100

    updated error handling

--- api/repositories/user.repository.js
+++ api/repositories/user.repository.js
@@ -2,16 +2,22 @@ import prisma from "../prisma/prisma.js";
 
 export const findUserByUsername = async (username) => {
   return prisma.users.findUnique({
-    where: {username}
+    where: {username},
+    select: {
+      id: true
+    }
   })
 }
 export const findUserByEmail = async (email) => {
   return prisma.users.findUnique({
-    where: {email}
+    where: {email},
+    select: {
+      id: true
+    }
   })
 }
 export const addUser = async (user) => {
-  await prisma.users.create({
+  return prisma.users.create({
     data: user
   })
 }
