165edc423e17a9616205622f038879c0e9df2b87
Mary Kelly <mk-km@live.com>
2024-05-27 12:36:00 -0400

    further style related setup

--- client-next/client/tailwind.config.ts
+++ client-next/client/tailwind.config.ts
@@ -13,6 +13,38 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        it: {
+          purple: {
+            100: "#C0C4F5",
+            200: "#9993EB",
+            300: "#7857E0",
+          },
+          teal: {
+            100: "#C9FFFA",
+            200: "#87E6DF",
+            300: "#36D4CA",
+          },
+          blue: {
+            100: "#CEE8FF",
+            200: "#5796D5",
+            300: "#2979E6",
+          },
+          gray: {
+            100: "#F3F7FA",
+            200: "#E5ECF2",
+            300: "#B2C5D4",
+            400: "#9BACB9",
+            500: "#6D7C87",
+          },
+          black: "#181A1A",
+        },
+      },
+      fontFamily: {
+        afacad: ["var(--font-afacad)"],
+        lexend: ["var(--font-lexend)"],
+        instrument: ["var(--font-instrument-sans)"],
+      },
     },
   },
   plugins: [],
