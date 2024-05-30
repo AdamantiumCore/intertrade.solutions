266c8a76ae1dc09a4e00b8827d1765b4d6520590
Gogi4aaa <georgi-07@abv.bg>
2024-05-30 22:42:56 +0300

    set up searchbar for use

--- client/src/components/common/SearchBar.tsx
+++ client/src/components/common/SearchBar.tsx
@@ -1,21 +1,34 @@
 "use client";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { useEffect } from "react";
+import { useState } from "react";
 type SearchBarProps = {
     placeholder: string,
 }
 export default function SearchBar({ placeholder }: SearchBarProps){
-    useEffect(() => {
-        console.log("It works");
-    }, []);
+    const [search, setSearch] = useState("");
+    function handleInputChange(e: any) {
+        setSearch(e.target.value);
+    }
+    function getData() {
+        //call BE for the data and pass search state
+    }
+    function keyDownHandler(e : any) {
+        let key = e.key;
+        if(key === "Enter"){
+            getData();
+        }
+    }
     return(
         <>
             <input
-              placeholder={placeholder}
-              className="h-full w-full rounded-full border-[1.5px] border-it-gray-400 pl-5 font-afacad outline-none placeholder:text-it-gray-300 focus:border-it-purple-200"
+                onKeyDown={keyDownHandler}
+                value={search}
+                onChange={handleInputChange}  
+                placeholder={placeholder}
+                className="h-full w-full rounded-full border-[1.5px] border-it-gray-400 pl-5 font-afacad outline-none placeholder:text-it-gray-300 focus:border-it-purple-200"
             />
-            <div className="absolute right-1 top-1 flex h-[32px] w-[32px] items-center justify-center rounded-full bg-it-purple-300 hover:bg-[#6944DC]">
-              <MagnifyingGlassIcon className="h-[22px] w-[22px] stroke-2 text-white" />
+            <div onClick={getData} className="absolute right-1 top-1 flex h-[32px] w-[32px] items-center justify-center rounded-full bg-it-purple-300 hover:bg-[#6944DC] hover:cursor-pointer">
+              <MagnifyingGlassIcon className="h-[22px] w-[22px] stroke-2 text-white " />
             </div>
         </>
     )
