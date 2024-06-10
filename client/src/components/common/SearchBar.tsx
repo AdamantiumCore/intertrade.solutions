2c2ea31ebc31d20caaaa2ff9ca085238a19fe861
META-RAG-101 <nalisyedn@gmail.com>
2024-06-10 21:32:40 +0100

    Update SearchBar.tsx

--- client/src/components/common/SearchBar.tsx
+++ client/src/components/common/SearchBar.tsx
@@ -49,27 +49,9 @@ export default function SearchBar({ placeholder }: SearchBarProps){
                 placeholder={placeholder}
                 className="h-full w-full rounded-full border-[1.5px] border-it-gray-400 pl-5 font-afacad outline-none placeholder:text-it-gray-300 focus:border-it-purple-200"
             />
-            <div onClick={getAllSearches} className="absolute right-1 top-1 flex h-[32px] w-[32px] items-center justify-center rounded-full bg-it-purple-300 hover:bg-[#6944DC] hover:cursor-pointer">
+            <div className="absolute right-1 top-1 flex h-[32px] w-[32px] items-center justify-center rounded-full bg-it-purple-300 hover:bg-[#6944DC] hover:cursor-pointer">
               <MagnifyingGlassIcon className="h-[22px] w-[22px] stroke-2 text-white " />
             </div>
-            <div>
-            Products:
-            {searchedData?.products.map((result, index) => {
-              return (
-                <div key={result.id} onClick={() => setSearch(result.name)}>
-                  {result.name}
-                </div>
-              );
-            })}
-            Stores:
-            {searchedData?.stores.map((result, index) => {
-              return (
-                <div key={result.id} onClick={() => setSearch(result.name)}>
-                  {result.name}
-                </div>
-              );
-            })}
-          </div>
         </>
     )
 }
